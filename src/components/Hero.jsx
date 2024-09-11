088b45055b69d2bdfe4c76e59d1e10af2ba7932b
Arun Shukla <69161719+anshu1016@users.noreply.github.com>
2024-09-11 10:34:15 +0530

    Revert "Update  the size of components"

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,54 +18,58 @@ const Hero = () => {
   };
 
   return (
-    <div className="border-b border-neutral-900 pb-4 lg:pb-0 h-screen flex flex-col justify-between">
-      <div className="flex flex-wrap items-center h-full">
-        <div className="w-full lg:w-1/2 px-4 lg:px-8 flex flex-col justify-center">
-          <motion.h1
-            variants={container(0)}
-            initial="hidden"
-            animate="visible"
-            className="text-5xl lg:text-6xl font-thin tracking-tight lg:mt-5 pb-8"
-          >
-            Arun Shukla
-          </motion.h1>
-          <motion.span
-            variants={container(0.5)}
-            initial="hidden"
-            animate="visible"
-            className="bg-gradient-to-r from-pink-300 via-slate-500 to-pink-500 bg-clip-text text-3xl lg:text-4xl tracking-tight text-transparent"
-          >
-            Frontend Engineer
-          </motion.span>
-          <motion.p
-            variants={container(1)}
-            initial="hidden"
-            animate="visible"
-            className="my-4 max-w-lg text-lg lg:text-xl font-light tracking-tighter"
-          >
-            {HERO_CONTENT}
-          </motion.p>
-          <motion.a
-            href="https://drive.google.com/file/d/181eQNULRGPX2YEtX0OY5ST-uztxfOEOd/view?usp=sharing"
-            download="Arun_Shukla_CV.pdf"
-            target="_blank"
-            variants={buttonVariants}
-            whileHover="hover"
-            whileTap="tap"
-            className="mt-6 px-6 py-2 bg-yellow-500 text-white text-lg font-semibold rounded-lg shadow-lg"
-          >
-            Download CV
-          </motion.a>
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="text-7xl lg:text-7xl font-thin tracking-tight lg:mt-10 pb-12"
+            >
+              Arun Shukla
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-pink-500 bg-clip-text text-5xl tracking-tight text-transparent"
+            >
+              Frontend Engineer
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 text-2xl font-light tracking-tighter"
+            >
+              {HERO_CONTENT}
+            </motion.p>
+            <motion.a
+              href="https://drive.google.com/file/d/181eQNULRGPX2YEtX0OY5ST-uztxfOEOd/view?usp=sharing" // Replace with the actual path to your CV
+              download="Arun_Shukla_CV.pdf"
+              target="_blank"
+              variants={buttonVariants}
+              whileHover="hover"
+              whileTap="tap"
+              className="mt-8 px-8 py-3 bg-yellow-500 text-white text-xl font-semibold rounded-lg shadow-lg transition-transform duration-300"
+            >
+              Download CV
+            </motion.a>
+          </div>
         </div>
-        <div className="w-full lg:w-1/2 p-4 flex justify-center lg:justify-end">
-          <motion.img
-            initial={{ x: 100, opacity: 0 }}
-            animate={{ x: 0, opacity: 1 }}
-            transition={{ duration: 1, delay: 1.1 }}
-            className="w-2/3 lg:w-full max-w-sm h-auto object-cover rounded-lg"
-            src={profilePic}
-            alt="profilePic"
-          />
+        <div className="w-full lg:w-1/2 p-8">
+          <div className="flex justify-center mt-10 lg:mt-0">
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.1 }}
+              className="w-full max-w-lg h-auto object-cover rounded-lg"
+              src={profilePic}
+              alt="profilePic"
+            />
+          </div>
         </div>
       </div>
     </div>
