328c3a7f8813a5474120f3753243308c0d6e631d
Arun Shukla <69161719+anshu1016@users.noreply.github.com>
2024-09-11 09:55:19 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,7 +34,7 @@ const Hero = () => {
               variants={container(0.5)}
               initial="hidden"
               animate="visible"
-              className="bg-gradient-to-r from-pink-300 via-slate-500 to-pink-500 bg-clip-text text-5xl tracking-tight text-transparent"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-pink-500 bg-clip-text text-5xl mb-2 tracking-tight text-transparent"
             >
               Frontend Engineer
             </motion.span>
